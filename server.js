b1123fdfcc81141ac986f49303a7a73f1a4ec3b0
Joey Hipolito <hi@joeyhipolito.com>
2014-09-06 19:46:46 +0800

    change to default whim exposed port 5000

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var server = http.createServer(function(req, res) {
     res.end('Hello World');
 });
 
-server.listen(8000);
+server.listen(5000);
 
-console.log("server running at http://0.0.0.0:8000");
+console.log("server running at http://0.0.0.0:5000");
